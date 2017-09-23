28c0efc40ea5049a5f33b8784407f08197d308a2
bennybenigno <bennybenigno@gmail.com>
2017-09-23 19:08:27 -0400

    Adding src folder

---
+++ src/com/team6668/frc2018/Robot.java
@@ -0,0 +1,61 @@
+package com.team6668.frc2018;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.team6668.frc2018.subsystems.Drive;
+
+public class Robot extendes IterativeRobot
+{
+	//run when the robot first started up and should be used for any initialization code
+	@Override
+	public void robotInit()
+	{
+		
+	}
+	
+	//called once the robot is in Disabled mode 
+	//you can use it to reset any subsystem information 
+	@Override 
+	public void disableInit()
+	{
+
+	}
+	
+	@Override
+	public void disabledPeriodic()
+	{
+
+	}
+
+	@Override
+	public void autonomousInit()
+	{
+	
+	}
+
+	@Overide 
+	public void autonomousPeriodic()
+	{
+
+	}
+
+	@Override
+	public void teleopInit()
+	{
+	}
+
+	@Override
+	public void teleopPeriodic()
+	{
+
+	}
+
+	@Override
+	public void testPeriodic()
+	{
+
+	}
+}
+
